7dea171ad8ff8fb681b5876b3933f200f9de6dfd
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-08-09 21:39:59 +0900

    Use middleware

--- app.ts
+++ app.ts
@@ -1,39 +1,42 @@
-import { Hono, Context } from "https://lib.deno.dev/x/hono@v3/mod.ts";
+import { Hono } from "https://lib.deno.dev/x/hono@v3/mod.ts";
 import { serveStatic } from "https://lib.deno.dev/x/hono@v3/middleware.ts";
 import { serve } from "https://deno.land/std@0.191.0/http/server.ts";
 import * as pod from "./pod.ts";
 
 const app = new Hono();
 
-app.get("/", (ctx) => ctx.redirect("/index.html") );
-
-function checkDate(ctx: Context, content: string) {
-  const date = content.match(/<meta name="date" content="(.*?)">/);
-  if (date) {
-    const now = new Date();
-    const postDate = new Date(date[1]);
-    if (postDate > now) {
-      return ctx.html("404 Not found", 404);
+app.use(async (ctx, next) => {
+  const ctx_html = ctx.html;
+  ctx.html = (content: string) => {
+    const date = content.match(/<meta name="date" content="(.*?)">/);
+    if (date) {
+      const now = new Date();
+      const postDate = new Date(date[1]);
+      if (postDate > now) {
+        return ctx_html("404 Not found", 404);
+      }
     }
+    return ctx_html(content);
   }
-  return ctx.html(content);
-}
+  await next();
+})
+
+app.get("/", (ctx) => ctx.redirect("/index.html") );
 
 app.get("/:file{.*\\.html}", (ctx) => {
   const file = ctx.req.param("file").replace(".html", ".pod");
   const source = Deno.readTextFileSync(`./pages/${file}`);
   const result = pod.process(source);
-  return checkDate(ctx, result);
+  return ctx.html(result);
 })
 
 app.get("/blog/:article{.*\\.html}", (ctx) => {
   const article = ctx.req.param("article").replace(".html", ".pod");
   const source = Deno.readTextFileSync(`./pages/blog/${article}`);
   const result = pod.process(source);
-  return checkDate(ctx, result);
+  return ctx.html(result);
 })
 
 app.get("/*", serveStatic({ root: './static' }));
-
 
 serve(app.fetch);
