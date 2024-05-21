03a302785c3e9fc431ac9447a0f8854e2f9f6c67
Masaya Taniguchi <ta2gch@gmail.com>
2024-05-21 10:49:04 +0900

    feat: Update font settings in head.php and uno.config.ts
    This commit includes changes in the font settings of the website. In
    head.php, new font links are added and the body and code font-family
    properties are updated. In uno.config.ts, the presetWebFonts function
    is removed from the presets array. This change is part of an effort to
    improve the website's typography and overall aesthetic.

--- uno.config.ts
+++ uno.config.ts
@@ -3,20 +3,12 @@ import {
   presetAttributify,
   presetTypography,
   presetUno,
-  presetWebFonts,
 } from "unocss";
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
-    presetTypography(),
-    presetWebFonts({
-      provider: 'google',
-      fonts: {
-        serif: ['Merriweather', 'Zen Old Mincho'],
-        mono: ['Fira Code']
-      },
-    }),
+    presetTypography()
   ]
 });
