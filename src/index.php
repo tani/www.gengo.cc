a98cfc9ee9acc0fbfadb5d9dc1b2840af75dcde2
Masaya Taniguchi <ta2gch@gmail.com>
2024-05-21 11:37:23 +0900

    Revert "fix: remove redundant attributes from body tag"
    This reverts commit 10a0d79acddf480feb7bdfb17f0804bec69f6098.

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@
     include 'head.php';
   ?>
 </head>
-<body p="sm:x-0 x-5" m="0" box-border un-cloak>
+<body bg="paper" font="serif" p="sm:x-0 x-5" m="0" font="sans" box-border un-cloak>
   <header m="x-auto" md:w="[calc(16rem+65ch)]">
     <h1 font="sans" bg="dark-900" text="white" inline-block p="x-2" m="y-0">GENGO.CC</h1><br>
     <div p="x-2">
